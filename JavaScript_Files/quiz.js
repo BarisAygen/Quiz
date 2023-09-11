ca3ebd6fedcca63a55ed1d938e12d400da03b008
Barış Aygen <hbaris@sabanciuniv.edu>
2023-09-11 13:53:39 +0300

    New Version

--- JavaScript_Files/quiz.js
+++ JavaScript_Files/quiz.js
@@ -1,3 +1,5 @@
+let score = 0;
+
 function Question (text, options, correct) {
     this.text = text;
     this.options = options;
@@ -5,13 +7,12 @@ function Question (text, options, correct) {
 }
 
 Question.prototype.check = function(answer) {
-    return answer === this.correct;
+    if(answer === this.correct) {
+        score++;
+        return true;
+    }
+    return false;
 }
-
-let q1 = new Question("Which one is the biggest animal?", {a: "African Elephant", b: "Blue Whale", c: "Polar Bear", 
-d: "Hippopotamus"}, "b");
-
-let q2 = new Question("What does 2+2 equals to?", {a: "0", b: "2", c: "4", d: "8"}, "c");
 
 let questions = [
     new Question("1-What year did the movie La La Land come out?", {a: "2016", b: "2014", c: "2018", d: "2013"}, "a"),
@@ -30,13 +31,69 @@ Quiz.prototype.bring = function() {
 }
 
 const quiz = new Quiz(questions);
+const optionList = document.querySelector(".option-list");
+const correction = '<div class="icon"><i class="fas fa-check"></i></div>';
+const incorrection = '<div class="icon"><i class="fas fa-times"></i></div>';
 
-document.querySelector("#start-button").addEventListener("click", function() {
-    if(quiz.questions.length != quiz.index) {
-        console.log(quiz.bring());
+document.querySelector(".btn-start").addEventListener("click", function() {
+    document.querySelector(".quiz-box").classList.add("active");
+    show(quiz.bring());
+    showQuestionNumber(quiz.index + 1, quiz.questions.length);
+    document.querySelector(".next-btn").classList.remove("show");
+});
+
+document.querySelector(".next-btn").addEventListener("click", function(){
+    if(quiz.questions.length != quiz.index + 1) {
+        document.querySelector(".quiz-box").classList.add("active");
         quiz.index += 1;
+        show(quiz.bring());
+        document.querySelector(".next-btn").classList.remove("show");
+        showQuestionNumber(quiz.index + 1, quiz.questions.length);
     }
     else {
-        console.log("Quiz is over...");
+        document.querySelector(".quiz-box").classList.remove("active");
     }
-})
+});
+
+function show(question) {
+    let ques = `<span>${question.text}</span>`
+    let ops = ``
+    for(let answer in question.options) {
+        ops += 
+        `
+            <div class="option">
+                <span><b>${answer}</b>: ${question.options[answer]}</span>
+            </div>
+        `;
+    }
+    document.querySelector(".question-text").innerHTML = ques;
+    optionList.innerHTML = ops;
+    const option = optionList.querySelectorAll(".option");
+
+    for(let opt of option) {
+        opt.setAttribute("onclick", "optionSelected(this)")
+    }
+}
+
+function optionSelected(opt) {
+    let ans = opt.querySelector("span b").textContent;
+    let ques = quiz.bring();
+    if(ques.check(ans)) {
+        opt.classList.add("correct");
+        opt.insertAdjacentHTML("beforeend", correction)
+    }
+    else {
+        opt.classList.add("incorrect");
+        opt.insertAdjacentHTML("beforeend", incorrection)
+    }
+    for(let i=0; i<optionList.children.length; i++) {
+        optionList.children[i].classList.add("disabled");
+    }
+
+    document.querySelector(".next-btn").classList.add("show");
+}
+
+function showQuestionNumber(queue, total) {
+    let tag = `<span class="badge bg-warning ">${queue} / ${total}</span>`;
+    document.querySelector(".quiz-box .question-index").innerHTML = tag;
+}
